62e72a739cb585282bdc749124eb0a78e3fe954a
Claire <clairefraser121@gmail.com>
2023-12-03 18:53:56 +0000

    added eventListener for cards

--- script/script.js
+++ script/script.js
@@ -1,2 +1,8 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    // Sample data for fitness goals
+    const fitnessGoals = [
+      { title: "Bulking", text: "Achieve your goal of gaining muscle mass and size with our bulking program.", image: "bulking-image.jpg", resultType: "bulk" },
+      { title: "Toning", text: "Define and sculpt your body with our toning program designed for a lean physique.", image: "toning-image.jpg", resultType: "lean" },
+      { title: "Cardio", text: "Maintain cardiovascular health with our cardio program for a healthy lifestyle.", image: "cardio-image.jpg", resultType: "lean" }
+    ];
   
